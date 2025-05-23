b845dedb7ac0a9eb8529e7763b19d616df8a3c48
Артем Задворный <artyom.zadvornyj@yandex.ru>
2025-05-23 16:36:43 +0300

    ДОбавлен swagger

--- backend/pki_system/urls.py
+++ backend/pki_system/urls.py
@@ -16,12 +16,28 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 from apps import users
 
 router = DefaultRouter()
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="Описание API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 api_v1_patterns = [
     path('auth/', include('apps.users.urls')),
@@ -31,6 +47,8 @@ api_v1_patterns = [
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/v1/', include(api_v1_patterns)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
